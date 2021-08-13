df40ce555df8b8f9e9c138478aeea0c637de5483
shivam chaurivar <schauriva21@gmail.com>
2021-08-13 12:00:58 +0530

    Update ai_practise.py

--- Self-Driving-Car/ai_practise.py
+++ Self-Driving-Car/ai_practise.py
@@ -1,2 +1,77 @@
+#AI for self Driving Car 
+
+#Importing Libaries
+import numpy as np
+import pandas as pd
+
+#Import pytorch
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import variable
+
+#Creating The architecture
+
+class Network(nn.Module):
+    
+    #Initialize the object
+    def __init__(self, input_size, nb_action ):     #input_size= number of input neurons, output_size=No of output neuron
+        super(Network, self).__init__()             #inherit from nn.module to use tools of module
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30 )                           #Full connection all hidden layer neurons connected to input(Liner) with hidden(2nd) layer
+        self.fc2 = nn.Linear(30, nb_action )                            #connection of hidden layer with output layer  
+        
+    #Activate the neuron i.e perform fwd propagation
+    def forward(self, state):  #input of neural net= state 
+        #Activate hidden neuron
+        x = F.relu(self.fc1(state, self))
+        #return output neuron
+        q_values = self.fc1(x)
+        return q_values
+    
+#Implement Experince Replay
+class 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+        
+    
+        
+    
 
 
